17617963d94b71226e528b06f8509ffa10739862
Utshab Saha <utshab.saha@accenture.com>
2025-01-15 14:52:16 +0800

    added hooks alternative

--- class-component-example/src/App.jsx
+++ class-component-example/src/App.jsx
@@ -12,6 +12,11 @@ class Welcome extends React.Component {
   }
 }
 
+// Hooks alternative
+const Hello = (props) => {
+  return <h1>Meet the { props.name }</h1>;
+}
+
 function App() {
   const name = 'StarGazers';
   return (
@@ -21,6 +26,7 @@ function App() {
           <Welcome name = "Stars"></Welcome>
           <Welcome name = { name }></Welcome>
           <Welcome name = "StarsAliens"></Welcome>
+          <Hello name = "StarsAliens-by-Hooks"></Hello>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         </hgroup>
       </article>
