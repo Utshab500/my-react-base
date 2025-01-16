23d68d90abe99a7ec0e655c6db637e8bf3980d12
Utshab Saha <utshab.saha@accenture.com>
2025-01-16 21:22:40 +0800

    modal example

---
+++ modal-example/src/App.jsx
@@ -0,0 +1,26 @@
+/**
+ * @description - This is an example of modals in a react app.
+ * 
+ * @author - Utshab
+ */
+import React, { Component, useState } from 'react';
+import ListCast from './components/ListCast';
+import Modals from './components/Modals';
+
+function App() {
+  const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null);
+  return (
+    <div className="container">
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <h1>Meet the Stargazers</h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
+          {/* When modal close is clicked handleClose would be called and setMemberInfo sets null in memberInfo */}
+        {memberInfo && <Modals member={memberInfo} handleClose={()=>{setMemberInfo(null)}} />}
+      </hgroup>
+    </div>
+  )
+}
+export default App
